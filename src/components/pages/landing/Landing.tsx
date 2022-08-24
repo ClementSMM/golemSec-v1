deec3814e51f63c8264877061062d17b97b96146
csaad <csaad@9troisquarts.com>
2022-08-24 02:13:36 +0200

    feat: landing page

---
+++ src/components/pages/landing/Landing.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./landing.css";
+
+const LandingPage = () => {
+  return(
+    <div className="golem-landing">
+      <div className="text-container">
+        <h1 className="title">GolemSec</h1>
+        <h2 className="subtitle">Création de site web</h2>
+        <p className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+      <div className="icon-container">
+        <div className="icon"></div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage;
