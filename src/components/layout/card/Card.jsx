9864ecd3c26d2f2f9c26d6c929faae12e01c9cc8
csaad <csaad@9troisquarts.com>
2022-08-25 01:33:04 +0200

    feat: about page

---
+++ src/components/layout/card/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const CardComponent = (props) => {
+  const { title, text } = props;
+  
+  return (
+    <div>
+      nfj'rnfzojj'rfeiomzje
+    </div>
+  );
+}
+
+export default CardComponent;
--- src/components/pages/landing/Landing.jsx
+++ src/components/pages/landing/Landing.jsx
@@ -4,7 +4,7 @@ import "./landing.css";
 const LandingPage = () => {
   return(
     <div className="golem-landing">
-      <div className="text-container">
+      <div className="landing-text-container">
         <h1 className="title">GolemSec</h1>
         <h2 className="subtitle">Création de site web</h2>
         <p className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
