ab93560638f1c68d7d45c7456053142d80badbca
Erwan Griffon <84179694+ErwanGriffon@users.noreply.github.com>
2022-08-25 14:26:58 +0200

    fix : navbar logo and position

--- src/components/pages/navbar/Navbar.jsx
+++ src/components/pages/navbar/Navbar.jsx
@@ -6,16 +6,17 @@ const LandingPage = () => {
 <header>
   <div class="wrapHead">
     <div id="brand">
-      <h1>GolemSec</h1>
     </div>
+    <div class="link">
     <nav>
       <ul>
-        <li><a href="#about" class="scroll">A Propos</a></li>
+        <li><a href="#about" class="scroll">À Propos</a></li>
         <li><a href="#service" class="scroll">Service</a></li>
         <li><a href="#pricing" class="scroll">Devis</a></li>
         <li><a href="#contact" class="scroll">Contact</a></li>
       </ul>
     </nav>
+    </div>
   </div>
 </header>
   )
