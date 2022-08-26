fb3a4f5c7cda12e458f8624e8a77e4253aa4101c
csaad <csaad@9troisquarts.com>
2022-08-26 11:23:17 +0200

    feat: quote page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import LandingPage from './components/pages/landing/Landing.jsx'
 import About from './components/pages/about/About.jsx';
 import Navbar from './components/pages/navbar/Navbar.jsx';
 import Services from './components/pages/services/Services.jsx'
+import Quote from './components/pages/quote/Quote.jsx'
 
 function App() {
   return (
     <div className="Golemec">
       <Navbar/>
-      <LandingPage />
+      {/* <LandingPage />
       <About/>
-      <Services/>
+      <Services/> */}
+      <Quote/>
     </div>
 
   );
