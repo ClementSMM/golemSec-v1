da8a1807a3d773a0c632563d209ccab855e6f929
csaad <csaad@9troisquarts.com>
2022-08-29 01:23:07 +0200

    feat responsive services

--- src/components/pages/services/Services.jsx
+++ src/components/pages/services/Services.jsx
@@ -4,38 +4,6 @@ import "./services.css"
 
 const Services = () => {
 
-/*   const textCards = [
-    {
-      title: "Service 1",
-      text: "1Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    },
-    {
-      title: "Service 2",
-      text: "2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    },
-    {
-      title: "Service 3",
-      text: "3Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    }
-  ]; */
-
-
-/*   const ServiceCard = (textCards) => {
-    return (
-      textCards.map(element => {
-        <div className="service-card-main">
-          <div className="title-service-card">
-            <h2>{element.title}</h2>
-          </div>
-          <div className="text-service-card">
-            <p>{element.text}</p>
-          </div>
-        </div>
-      })
-
-    )
-  } */
-
   return (
     <div id="service" className="golem-services">
       
