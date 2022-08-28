f607b0c6abcc94a96d76396cbf87ea58d37894b4
csaad <csaad@9troisquarts.com>
2022-08-28 02:36:54 +0200

    fix: navBar trensparence

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import LandingPage from './components/pages/landing/Landing.jsx'
 import About from './components/pages/about/About.jsx';
-import Navbar from './components/pages/navbar/Navbar.jsx';
+import Navbar from './components/layout/navbar/Navbar.jsx';
 import Services from './components/pages/services/Services.jsx'
 import Contact from './components/pages/contact/Contact.jsx'
 
